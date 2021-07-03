c182410f4962adc4737cdcc56c8abf7ced9ef465
hecterbonha <untungsoandryanto@gmail.com>
2021-07-03 15:39:01 +0700

    integrate deploy

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Home;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        $data = ['message' => 'Welcome to Malah Ngoding!'];
+        return response()->json($data, 200);
+    }
+
+    public function keep_awake()
+    {
+        $wake = Home::awake();
+        $data = ['message' => 'Successfully woke!', 'data' => $wake];
+        return response()->json($data, 200);
+    }
+}
---
+++ app/Models/Home.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Home extends Model
+{
+    use HasFactory;
+
+    static public function awake()
+    {
+        return DB::table('migrations')->get();
+    }
+}
