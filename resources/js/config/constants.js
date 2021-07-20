3c3929b1c70d4dac195c425fe9179417db463757
hecterbonha <untungsoandryanto@gmail.com>
2021-07-20 10:07:20 +0700

    init

--- resources/js/config/constants.js
+++
@@ -1,9 +0,0 @@
-const constants = {
-  HOST_URL: 'http://localhost:8000/api',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-};
-
-export default constants;
