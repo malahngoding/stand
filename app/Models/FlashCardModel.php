fa18728ffa63a4981212947d570cb6904a57fffd
dennyrismanto <rismanto.denny@gmail.com>
2021-08-21 17:25:11 +0700

    Init

--- app/Models/FlashCardModel.php
+++ app/Models/FlashCardModel.php
@@ -9,14 +9,24 @@ use Illuminate\Support\Facades\DB;
 class FlashCardModel extends Model
 {
     use HasFactory;
-    public static function getData()
+    public static function getData($NumberGroupName)
     {
 
-        $user = DB::table('questions')
+        $user = DB::table('questions')->where('questions.id', $NumberGroupName)
             ->join('quiz_question', 'questions.id', '=', 'quiz_question.quizgroup_id')
             ->select('questions.groupname','quiz_question.quizgroup_id','quiz_question.picture','quiz_question.score','quiz_question.question','quiz_question.correct_answer','quiz_question.incorrect_answer')
             ->get();
 
         return $user;
     }
+    // public static function getQuizGroup($email)
+    // {
+
+    //     $quizGroup = DB::table('users')->where('email', $email)
+    //         ->join('profile', 'users.id', '=', 'profile.users_id')
+    //         ->select('users.id', 'users.name', 'users.email', 'users.email_verified_at', 'users.remember_token', 'profile.users_id', 'profile.users_name', 'profile.total_flash_card', 'profile.total_guide_number', 'profile.last_achievement_item', 'profile.favorite_article', 'users.created_at', 'users.updated_at')
+    //         ->get();
+
+    //     return $quizGroup;
+    // }
 }
