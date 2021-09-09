9851c05cac8dd441834ef66ff838e204b64f5cf2
hecterbonha <untungsoandryanto@gmail.com>
2021-09-09 23:39:39 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::redirect('/', config('app.frontend_url'));
 Route::redirect('/', config('app.frontend_url'));
 
 require __DIR__.'/auth.php';
