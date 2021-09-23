93bc491237c673a813590b4540a1d160417f8c6c
Sulthan Taqi Rabbani <sulthanrabbanitaqi@gmail.com>
2021-09-23 21:37:12 +0700

    Update QuizController.php

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\QuizModel;
+
+
+class QuizController extends Controller
+{
+    public function getQuizGroupName()
+    {
+        #test push
+    }
+}
