9ea2e8ce9804534e1f4134f25c80942a166863c8
Sulthan Taqi Rabbani <sulthanrabbanitaqi@gmail.com>
2021-09-23 21:49:43 +0700

    Refactoring To Feature CMS FlashCard Model

---
+++ app/Models/FlashCardModel.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class FlashCardModel extends Model
+{
+    use HasFactory;
+    public static function getData($id)
+    {
+        #MOVE TO FEATURE CMS BRANCH
+
+
+        $user = DB::table('quiz_question_group')->where('quiz_question_group.id', $id)
+            ->join('quiz_question', 'quiz_question_group.id', '=', 'quiz_question.quizgroup_id')
+            ->select('quiz_question_group.groupname', 'quiz_question.quizgroup_id', 'quiz_question.picture', 'quiz_question.score', 'quiz_question.question', 'quiz_question.correct_answer', 'quiz_question.incorrect_answer')
+            ->get();
+
+        return $user;
+    }
+    public static function getQuizGroupName($email)
+    {
+        $get = DB::table('questions_flow')
+            ->where('questions_flow.email', $email)
+            ->join('quiz_question_group', 'questions_flow.QuizGroup', '=', 'quiz_question_group.id')
+            ->select('quiz_question_group.id', 'quiz_question_group.groupname', 'questions_flow.email', 'questions_flow.noQuiz', 'questions_flow.QuizGroup')
+            ->get();
+
+        return $get;
+    }
+
+    public static function checkEmailOnUsersAndQuizResult($email)
+    {
+        $get = DB::table('users')
+            ->where('email', $email)
+            ->get();
+        $get2 = DB::table('quiz_result')
+            ->where('email', $email)
+            ->get();
+        return $get;
+    }
+    public static function insertEmail($email)
+    {
+        $get = DB::table('users')
+            ->updateOrInsert([
+                'email' => $email,
+            ]);
+        $get2 = DB::table('quiz_result')
+            ->updateOrInsert([
+                'email' => $email,
+            ]);
+        return $get;
+    }
+
+    public static function getDataTotalScore($email)
+    {
+
+        $user = DB::table('quiz_result')->where('email', $email)->first();
+
+        return $user;
+    }
+
+    public static function postDataResult($email, $nilai, $jawaban_benar, $akurasi, $rata_rata, $totalscore, $soal_dilewati, $QuizGroup)
+
+    {
+        DB::table('quiz_result')->where('email', $email)->update([
+            'nilai' => $nilai,
+            'jawaban_benar' => $jawaban_benar,
+            'akurasi' => $akurasi,
+            'rata_rata' => $rata_rata,
+            'soal_dilewati'  =>  $soal_dilewati,
+            'totalscore' => $totalscore,
+        ]);
+
+        DB::table('questions_flow')->where('email', $email)->update([
+            'QuizGroup' => $QuizGroup,
+        ]);
+    }
+
+    public static function getQuizResult($email)
+    {
+        $get = DB::table('quiz_result')
+            ->where('email', $email)
+            ->first();
+
+        return $get;
+    }
+}
---
+++ app/Models/ProfileModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class ProfileModel extends Model
+{
+    use HasFactory;
+    public static function getProfile($email)
+    {
+        #MOVE TO FEATURE CMS BRANCH
+
+
+        $user = DB::table('users')->where('email', $email)
+            ->join('profile', 'users.id', '=', 'profile.users_id')
+            ->select('users.id', 'users.name', 'users.email', 'users.email_verified_at', 'users.remember_token', 'profile.users_id', 'profile.users_name', 'profile.total_flash_card', 'profile.total_guide_number', 'profile.last_achievement_item', 'profile.favorite_article', 'users.created_at', 'users.updated_at')
+            ->get();
+
+
+        return $user;
+    }
+}
