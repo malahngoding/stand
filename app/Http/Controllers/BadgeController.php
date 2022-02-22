f686b5cb8a884a85e4aa3c2bb09de7c49a4e3199
Sulthan Taqi Rabbani <sulthanrabbanitaqi@gmail.com>
2022-02-22 18:49:22 +0700

    feat(badge): awsm-noob lvl 5

--- app/Http/Controllers/BadgeController.php
+++ app/Http/Controllers/BadgeController.php
@@ -9,9 +9,28 @@ class BadgeController extends Controller
 {
     public function getBadge(Request $request)
     {
-        $get = BadgeModel::getBadge();
-        return response()->json($request->who);
+        $who = $request->who;
+        $get = BadgeModel::getBadge($who);
+
+        if (count($get) === 0) {
+            $insert = BadgeModel::newUserBadge($who);
+            return response()->json(true);
+        } else {
+            return response()->json($get);
+        }
+    }
+
+    public function awsmLevel5(Request $request)
+    {
+        $who = $request->who;
+        $badge = $request->badge_id;
+        $get = BadgeModel::level5Awsm($who, $badge);
+
+        if (count($get) === 0) {
+            $insert = BadgeModel::awsmLevel5Badge($who, $badge);
+            return response()->json(true);
+        } else {
+            return response()->json($get);
+        }
     }
 }
-
-
--- app/Models/BadgeModel.php
+++ app/Models/BadgeModel.php
@@ -10,11 +10,47 @@ class BadgeModel extends Model
 {
     use HasFactory;
 
-    public static function getBadge()
+    public static function getBadge($who)
     {
-        $data = DB::table('badge')
+        $data = DB::table('badge_association')
+            ->join('badge', 'badge_association.badge_uuid', '=', 'badge.id')
+            ->where('badge_association.users_uuid', $who)
             ->get();
         return $data;
     }
-
+    public static function level5Awsm($who, $badge)
+    {
+        $data = DB::table('badge_association')
+            ->join('badge', 'badge_association.badge_uuid', '=', 'badge.id')
+            ->where('badge_association.users_uuid', $who)
+            ->where('badge_association.badge_uuid', $badge)
+            ->get();
+        return $data;
+    }
+    public static function awsmLevel5Badge($who, $badge)
+    {
+        $created_at = now();
+        $updated_at = now();
+        $data = DB::table('badge_association')
+            ->insert([
+                'users_uuid' => $who,
+                'badge_uuid' => $badge,
+                'created_at' => $created_at,
+                'updated_at' => $updated_at,
+            ]);
+        return $data;
+    }
+    public static function newUserBadge($who)
+    {
+        $created_at = now();
+        $updated_at = now();
+        $data = DB::table('badge_association')
+            ->insert([
+                'users_uuid' => $who,
+                'badge_uuid' => 1,
+                'created_at' => $created_at,
+                'updated_at' => $updated_at,
+            ]);
+        return $data;
+    }
 }
